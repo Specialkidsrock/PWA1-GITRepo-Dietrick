681cfecd6e3edea42a56af9fc4b8240ee774fc72
Specialkidsrock <tldietrick@fullsail.edu>
2015-02-09 15:19:36 -0500

    Goal 1 Duel 1 damage variables created
    Created minDamage variables for both characters and calculated the min
    damage using the math.random and math.floor calculation.

--- Wk 1/goal1_duel1/js/main.js
+++ Wk 1/goal1_duel1/js/main.js
@@ -25,9 +25,25 @@
     var round = 1;                                          //Variable assigned to keep track of the rounds of the fight
 
 
-    function fight(){                                       //Declares the function that will be used but does not execute until called
-        console.log("In the fight function");
-        winnerCheck()
+    function fight() {                                       //Declares the function that will be used but does not execute until called
+        console.log("In the fight function");               //Prints to the console log in order to show the js is
+
+                                                            //An alert that prompts the user the information on the players name and health using string concatenation with global variables
+
+        alert(playerOneName + ":" + playerOneHealth + " ** START ** " + playerTwoName + ":" + playerTwoHealth);
+
+        for (var i = 0; i < 10; i++) {                      //Creates a for loop to increment the round number up to round 10
+
+
+            var minDamage1 = player1Damage * .5;            //Creates a variable that defines minimum damage that can be incurred to the other player
+            var minDamage2 = player2Damage * .5;            //Creates a variable that defines minimum damage that can be incurred to the other player
+
+            var f1 = Math.floor(Math.random() * (player1Damage - minDamage1) + minDamage1);     //random formula is - Math.floor(Math.random() * (max - min) + min);
+            var f2 = Math.floor(Math.random() * (player2Damage - minDamage2) + minDamage2);     //math.random generates a decimal number between 0 an 1
+                                                                                                //(max - min) + min then is used to generate a number between the min and max and the math.floor rounds it to an integer
+            console.log(f1);
+            console.log(f2);
+        }
     };
 
     function winnerCheck(){
