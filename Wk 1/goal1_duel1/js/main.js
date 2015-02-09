e98a305923831bc816b440aefbb6ec3f7b1b647b
Specialkidsrock <tldietrick@fullsail.edu>
2015-02-09 14:47:11 -0500

    Goal 1 Duel 1 Variables created
    Player, health, damage, and round variables all created and annotated
    with proper pseudo code

--- Wk 1/goal1_duel1/js/main.js
+++ Wk 1/goal1_duel1/js/main.js
@@ -9,4 +9,21 @@
 
 (function(){
     console.log("Fight!!!");
+
+    //player name
+    var playerOneName = "Superman";                         //Variable assigned the name "Superman"
+    var playerTwoName = "DoomsDay";                         //Variable assigned the name "Doomsday"
+
+    //player damage
+    var player1Damage = 30;                                 //Variable assigned to player1 as maximum damage it can incur on the other player each round
+    var player2Damage = 30;                                 //Variable assigned to player2 as maximum damage it can incur on the other player each round
+
+    //player health
+    var playerOneHealth = 100;                              //Variable assigned as the starting max health of player 1
+    var playerTwoHealth = 100;                              //Variable assigned as the starting max health of player 2
+
+    var round = 1;                                          //Variable assigned to keep track of the rounds of the fight
+
+    
+
 })();
