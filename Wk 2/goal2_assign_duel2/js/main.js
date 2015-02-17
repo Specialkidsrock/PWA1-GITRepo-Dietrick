1993136f895baa53a070fc3efe52752737b83f9f
Specialkidsrock <tldietrick@fullsail.edu>
2015-02-16 19:10:39 -0500

    Goal 2 Duel 2 updated
    Goal 2 Duel 2 updated to show use of arrays and works.

--- Wk 2/goal2_assign_duel2/js/main.js
+++ Wk 2/goal2_assign_duel2/js/main.js
@@ -1,8 +1,8 @@
 /*
  Name: Terry Dietrick
- Date: 08 Feb 2015
+ Date: 15 Feb 2015
  Class & Section:  PWA1-Sec 01
- Comments: "Goal 1 Duel"
+ Comments: "Goal 2 Duel 2"
  */
 
 // self-executing function
@@ -11,16 +11,19 @@
     console.log("Fight!!!");                               //Writes out a message to the console in the browser to verify js source functionality with html file
 
     //player name
-    var playerOneName = "Superman";                         //Variable assigned the name "Superman"
-    var playerTwoName = "DoomsDay";                         //Variable assigned the name "Doomsday"
+    var playerOnes = ["Superman", 30, 100];                 //Array to store player 1 name, damage and health
+    var playerTwos = ["Doomsday", 30, 100];                 //Array to store player 2 name, damage and health
+
+    //var playerOneName = "Superman";                         //Variable assigned the name "Superman"
+    //var playerTwoName = "DoomsDay";                         //Variable assigned the name "Doomsday"
 
     //player damage
-    var player1Damage = 30;                                 //Variable assigned to player1 as maximum damage it can incur on the other player each round
-    var player2Damage = 30;                                 //Variable assigned to player2 as maximum damage it can incur on the other player each round
+    //var player1Damage = 30;                                 //Variable assigned to player1 as maximum damage it can incur on the other player each round
+    //var player2Damage = 30;                                 //Variable assigned to player2 as maximum damage it can incur on the other player each round
 
     //player health
-    var playerOneHealth = 100;                              //Variable assigned as the starting max health of player 1
-    var playerTwoHealth = 100;                              //Variable assigned as the starting max health of player 2
+    //var playerOneHealth = 100;                              //Variable assigned as the starting max health of player 1
+    //var playerTwoHealth = 100;                              //Variable assigned as the starting max health of player 2
 
     var round = 0;                                          //Variable assigned to keep track of the rounds of the fight
 
@@ -30,23 +33,23 @@
 
                                                             //An alert that prompts the user the information on the players name and health using string concatenation with global variables
 
-        alert(playerOneName + " : " + playerOneHealth + " ** START ** " + playerTwoName + " : " + playerTwoHealth);
+        alert(playerOnes[0] + " : " + playerOnes[2] + " ** START ** " + playerTwos[0] + " : " + playerTwos[2]);
 
         for (var i = 0; i < 10; i++) {                      //Creates a for loop to increment the round number up to round 10
 
 
-            var minDamage1 = player1Damage * .5;            //Creates a variable that defines minimum damage that can be incurred to the other player
-            var minDamage2 = player2Damage * .5;            //Creates a variable that defines minimum damage that can be incurred to the other player
+            var minDamage1 = playerOnes[2] * .5;            //Creates a variable that defines minimum damage that can be incurred to the other player
+            var minDamage2 = playerTwos[2] * .5;            //Creates a variable that defines minimum damage that can be incurred to the other player
 
-            var f1 = Math.floor(Math.random() * (player1Damage - minDamage1) + minDamage1);     //random formula is - Math.floor(Math.random() * (max - min) + min);
-            var f2 = Math.floor(Math.random() * (player2Damage - minDamage2) + minDamage2);     //math.random generates a decimal number between 0 an 1
+            var f1 = Math.floor(Math.random() * (playerOnes[1] - minDamage1) + minDamage1);     //random formula is - Math.floor(Math.random() * (max - min) + min);
+            var f2 = Math.floor(Math.random() * (playerTwos[1] - minDamage2) + minDamage2);     //math.random generates a decimal number between 0 an 1
                                                                                                 //(max - min) + min then is used to generate a number between the min and max and the math.floor rounds it to an integer
             //console.log(f1);                              //prints to the console to verify the random numbers are being generated
             //console.log(f2);                              //prints to the console to verify the random numbers are being generated
 
             //inflict damage
-            playerOneHealth -= f1;                          //subtracts the random damage number from the player 1 health (same as playerOneHealth = playerOneHealth - f1)
-            playerTwoHealth -= f2;                          //subtracts the random damage number from the player 1 health (same as playerOneHealth = playerOneHealth - f1)
+            playerOnes[2] -= f1;                          //subtracts the random damage number from the player 1 health (same as playerOneHealth = playerOneHealth - f1)
+            playerTwos[2] -= f2;                          //subtracts the random damage number from the player 1 health (same as playerOneHealth = playerOneHealth - f1)
 
             //console.log(playerOneHealth);
             //console.log(playerTwoHealth);
@@ -59,7 +62,7 @@
 
             if(results === "no winner"){                                 //conditional to check and see if no winner has been found
                 round ++;                                                //increments the round by 1
-                alert(playerOneName + " : " + playerOneHealth + " Round : " + round + " OVER " + playerTwoName + " : " + playerTwoHealth);      //prompts the user the current player health and round number
+                alert(playerOnes[0] + " : " + playerOnes[2] + " Round : " + round + " OVER " + playerTwos[0] + " : " + playerTwos[2]);      //prompts the user the current player health and round number
             }else{
                 alert(results);                             //displays the winner in a prompt to the user if there is one
                 break;                                      //breaks out of for loop and exits the js
@@ -74,14 +77,14 @@
 
         var results = "no winner";                          //declares the variable and gives it the string value of no winner
 
-        if(playerOneHealth < 1 && playerTwoHealth < 1){     //conditional to heck and see if either players health is less then 1
+        if(playerOnes[2] < 1 && playerTwos[2] < 1){     //conditional to heck and see if either players health is less then 1
             results = "You both Die!"
 
-        }else if(playerOneHealth < 1){                      //conditional to check and see if player 1's health is less then 1 resulting in player 2 winning
-            results = playerTwoName + " Wins!!! "
+        }else if(playerOnes[2] < 1){                      //conditional to check and see if player 1's health is less then 1 resulting in player 2 winning
+            results = playerOnes[0] + " Wins!!! "
 
-        }else if(playerTwoHealth < 1){                      //conditional to check and see if player 2's health is less then 1 resulting in player 1 winning
-            results = playerTwoName + " Wins!!! "
+        }else if(playerTwos[2] < 1){                      //conditional to check and see if player 2's health is less then 1 resulting in player 1 winning
+            results = playerOnes[0] + " Wins!!! "
         };
 
         return results;
